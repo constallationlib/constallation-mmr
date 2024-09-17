e2930a14a32222bfac5b1cf136e5540edb15890c
CoulterStutz <coultercash@gmail.com>
2024-09-17 14:50:36 -0600

    INIT

---
+++ constallation_mmr/__init__.py
@@ -0,0 +1,5 @@
+__name__ = "constallation_mmr"
+__version__ = "1.0.0"
+__author__ = "Coulter Stutz"
+__email__ = "coulterstutz@constallation.wiki"
+import rigs
--- constallation_mmr/rigs/__init__.py
+++ constallation_mmr/rigs/__init__.py
@@ -1,6 +1,2 @@
-__name__ = "constallation_mmr"
-__version__ = "1.0.0"
-__author__ = "Coulter Stutz"
-__email__ = "coulterstutz@constallation.wiki"
 from .rig import Rig, fetch_rigs
 __all__ = ["Rig", "fetch_rigs"]
