eea8eee665b4b851534a23c9111b717b33cdade4
CoulterStutz <coultercash@gmail.com>
2024-09-17 15:49:22 -0600

    reflect new vers 1.0.2

--- constallation_mmr/__init__.py
+++ constallation_mmr/__init__.py
@@ -3,4 +3,4 @@ __version__ = "1.0.2"
 __author__ = "Coulter Stutz"
 __email__ = "coulterstutz@constallation.wiki"
 from .rig import *
-__all__ = ["Rig", "fetch_rigs"]
+__all__ = ["Rig", "fetch_rigs", "fetch_rig"]
