36f467fb710514ad76493b064640d06b68ac59e7
CoulterStutz <coultercash@gmail.com>
2024-09-17 14:49:07 -0600

    INIT

--- constallation_mmr/rigs/rig.py
+++ constallation_mmr/rigs/rig.py
@@ -155,6 +155,22 @@ class Rig:
         return self._data.get("rented")
 
 
+
+def fetch_rigs(rig_ids:list[int], rigs_refresh_rate:int=5):
+    """
+    Fetches multiple rigs and returns them as constallation_mmr.Rig objects.
+    :param rig_ids: A list of rigs to query.
+    :param rigs_refresh_rate: The interval of which the rigs autorefresh
+    :return: list of Rigs
+    """
+    rigs = []
+    for _ in rig_ids:
+        _rig = Rig(_, rigs_refresh_rate)
+        rigs.append(_rig)
+
+    return rigs
+
+
 if __name__ == "__main__":
     rig = Rig(rig_id=328771)
     print(rig.name)
